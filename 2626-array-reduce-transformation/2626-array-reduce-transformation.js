eb0eed407683a983a6ad53db62f71e6fe8764a79
AJITH A <arunmon4444@gmail.com>
2026-01-14 22:28:34 +0530

    Time: 36 ms (94.65%), Space: 55.2 MB (51.24%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -4,12 +4,12 @@
  * @param {number} init
  * @return {number}
  */
-var reduce = function(nums, fn, init) {
-   let result=init;
-   for(let i = 0 ; i < nums.length ; i ++){
-    result=fn(result,nums[i])
-   }
+var reduce = function (nums, fn, init) {
+    let result = init;
+    for (let i = 0; i < nums.length; i++) {
+        result = fn(result, nums[i])
+    }
 
-   return result
-    
+    return result
+
 };
