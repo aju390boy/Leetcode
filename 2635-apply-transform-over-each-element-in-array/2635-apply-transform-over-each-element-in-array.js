3b2a7e6ce770ba6dfac1fccf6ce8cd413261f375
AJITH A <arunmon4444@gmail.com>
2026-01-20 12:53:27 +0530

    Time: 45 ms (51.55%), Space: 53.3 MB (81.95%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,11 +4,12 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    let result=[];
+   let result =[];
     for(let i=0;i<arr.length;i++){
         result.push(fn(arr[i],i));
     }
 
     return result
-    
+
+   
 };
