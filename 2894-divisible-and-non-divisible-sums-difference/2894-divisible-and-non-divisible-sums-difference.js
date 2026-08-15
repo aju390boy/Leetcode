7bf1b0f63c72596b2d3030238c0453e99f53e0ed
AJITH A <arunmon4444@gmail.com>
2026-08-15 13:50:21 +0530

    Time: 0 ms (100%), Space: 57 MB (11.92%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -4,20 +4,15 @@
  * @return {number}
  */
 var differenceOfSums = function(n, m) {
-    let num1=0;
-    let num2=0;
+    let sum1=0;
+    let sum2=0;
 
-    for(i=1;i<=n;i++){
+    for(let i=1;i<=n;i++){
         if(i%m==0){
-            num1+=i;
+            sum1+=i;
         }else{
-            num2+=i;
+         sum2+=i;
         }
     }
-    console.log(`num1 ${num1}`);
-        console.log(`num2 ${num2}`)
-
-
-    return num2-num1
-    
+    return sum2-sum1;
 };
