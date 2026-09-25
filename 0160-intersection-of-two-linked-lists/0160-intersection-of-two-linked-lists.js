372042977d252d5a19f4ae701e97425288fb82c4
AJITH A <arunmon4444@gmail.com>
2026-09-25 20:28:07 +0530

    Time: 78 ms (15.48%), Space: 65.1 MB (92.04%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let lista = headA;
+    let listb = headB;
+
+    while (lista !== listb) {
+        lista = lista ? lista.next : headB;
+        listb = listb ? listb.next : headA;
+    }
+
+    return lista;    
+};
